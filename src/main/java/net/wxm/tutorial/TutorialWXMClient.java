75b5e9257ba35b5b087a4715c663d078ea8e951c
WIXIMERS <deracok11@gmail.com>
2024-11-30 17:07:40 +0100

    innitial commit

---
+++ src/main/java/net/wxm/tutorial/TutorialWXMClient.java
@@ -0,0 +1,10 @@
+package net.wxm.tutorial;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialWXMClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
